f5a20214de011ece50251c6ed4d197d76551c37a
Wealthometer <wealthhajoh87@gmail.com>
2025-06-21 21:12:59 +0100

    Update

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts')
 
+const connectDB = require('./server/config/db')
+
+//Connectin to DB
+connectDB();
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
