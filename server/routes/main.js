b971b14d06918cfdc07613492debc85fe85dfb82
Wealthometer <wealthhajoh87@gmail.com>
2025-06-21 03:24:46 +0100

    Update Main.js

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,6 +4,13 @@ const router = express.Router();
 //Routes
 
 router.get('', (req, res) => {
+    // How to render or pass ejs data
+    const locals = {
+        title: "Nodejs Blog",
+        description: "Simple Blog Created"
+    }
+
+
     res.render('index')
 })
 
