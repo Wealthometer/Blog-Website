b4a427213e3783c13c04be7678f953af17d25c26
Wealthometer <wealthhajoh87@gmail.com>
2025-06-23 11:36:07 +0100

    Update

--- app.js
+++ app.js
@@ -2,14 +2,34 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
-
-//Connectin to DB
-connectDB();
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
+  
+// Connect to DB
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
 
 app.use(express.static('public'));
 
@@ -18,14 +38,13 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-// app.get('', (req, res) => {
-//     res.send(`<h1>Hello World</h1>`);
-// })
 
-app.use('/', require("./server/routes/main"));
+app.locals.isActiveRoute = isActiveRoute; 
+
+
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT, ()=> {
-    console.log(`App Lisening on Port ${PORT}`)
-})
-
-// npm run dev . to make sure the express sever is working
+  console.log(`App listening on port ${PORT}`);
+});
--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
+  
+  try {
+    mongoose.set('strictQuery', false);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Database Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
 
-    try {
-        mongoose.set('strictQuery', false);
-        const conn =  await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Database Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log("DB Connection Failed: " + error);
-    }
-    
 }
 
 module.exports = connectDB;
